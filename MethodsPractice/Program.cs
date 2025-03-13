ce60daa95dcbbfa247fe7f8f4487b65f381df9b0
Veronika Niema <113263850+Veronika2007931@users.noreply.github.com>
2025-03-13 10:11:59 +0200

    add

---
+++ MethodsPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+Console.SetCursorPosition(20, 20);
+Console.WriteLine("Hello KP-41!");
+
+int X = Console.CursorLeft;
+int y = Console.CursorTop;
+
+Console.WriteLine($"{X} {y}");
+
+
+
+Console.SetWindowSize(40, 40);
+Console.WriteLine(Console.WindowWidth);
+Console.WriteLine(Console.WindowHeight);
+
+Console.SetBufferSize(80, 20);
+
+Console.WriteLine("Enter one symbol");
+int asciiCode = Console.Read();
+char charcter = (char)asciiCode;
+Console.WriteLine($"You entered symbol {charcter}");
+
+while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+
+Console.ReadKey(true);
+Console.ReadKey(false);
+// Щоб або виводити на екран або не виводити
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Ukraine!!");
+
+Console.ResetColor();
+Console.Clear();
+// очищує консоль
+Boolean p = Console.CursorVisible;
+Console.CursorVisible = true;
+Console.CursorVisible = false;
+Console.CursorSize = 20;
